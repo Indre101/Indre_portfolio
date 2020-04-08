ebc19de3313171f19e0e882ca1b39e0af3bda51f
indre101 <zygaityte.indre@gmail.com>
2020-04-08 19:05:56 +0200

    adds different definition

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import ProjectPage from "./components/ProjectPage";
 import ProjectsPage from "./components/ProjectsPage";
 import CloseProjectBtn from "./components/CloseProjectBtn";
 import AboutMe from "./components/AboutMe";
-import data from "./data";
 
 class App extends Component {
   constructor() {
@@ -32,11 +31,11 @@ class App extends Component {
   }
 
   componentDidMount() {
-    // fetch("projects.json")
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    this.setState({ projects: data });
-    // });
+    fetch("projects.json")
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ projects: data });
+      });
   }
 
   toggleProjectPage() {
